b1a11d0ba11264725bb85481f2a76c4a92325278
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2022-06-22 18:23:20 +0200

    Create Entries Model

--- entries/models.py
+++ entries/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Entry(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_created = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name_plural = "Entries"
