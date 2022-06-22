a5dbbcf8a86656a87dbc3a5eb05cf23a0821245a
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2022-06-22 19:20:50 +0200

    Add views and templates

--- entries/views.py
+++ entries/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import (ListView, DetailView)
+from .models import Entry
 
-# Create your views here.
+class EntryListView(ListView):
+    model = Entry
+    queryset = Entry.objects.all().order_by("-day-created")
+    
+class EntryDetailView(DetailView):
+    model = Entry
