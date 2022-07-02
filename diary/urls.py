8ec64d4185bbf64c4352352a308494541baa982e
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2022-07-02 15:30:37 +0300

    Add template and register home url

--- diary/urls.py
+++ diary/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("entries.urls")),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('home/', include('users.urls'))
 ]
