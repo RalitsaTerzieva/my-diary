cac2d38099d786a70ca87f11a0072abccf32571a
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2022-07-03 10:51:54 +0300

    Fix urls error

--- diary/settings.py
+++ diary/settings.py
@@ -124,4 +124,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
-AUTH_USER_MODEL = 'accounts.User'
+# AUTH_USER_MODEL = 'users.User'
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,"users/home.html")
+    return render(request, "users/home.html")
+ 
