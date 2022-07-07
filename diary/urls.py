4a668a7b8dbb6fbcdf93d55a48f688c002da3303
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2022-07-07 20:26:00 +0300

    Add bootstrap and font awesome links

--- diary/urls.py
+++ diary/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', include('users.urls')),
     path("",include("entries.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('home/', include('users.urls'))
+    
 ]
