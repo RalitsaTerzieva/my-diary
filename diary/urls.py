5e130b3f3dc69eb728dc0e17fbc1abb6daa7625d
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2022-07-10 12:21:24 +0300

    Style  navbar and footer

--- diary/urls.py
+++ diary/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from diary.views import index
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,4 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('users.urls'))
     
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
