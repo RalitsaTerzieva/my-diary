cd61616f21df6ca79da8c2fa52f383078768a763
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2022-07-31 18:36:33 +0300

    Add validation message

--- entries/forms.py
+++ entries/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class EntryForm(forms.Form):
-    title = forms.CharField()
-    content = forms.CharField(widget=forms.Textarea)
+    title = forms.CharField(min_length=5, max_length=255, required=True)
+    content = forms.CharField(min_length=10, max_length=1024, widget=forms.Textarea, required=True)
--- users/views.py
+++ users/views.py
@@ -1,9 +1,12 @@
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.messages.views import SuccessMessageMixin
 
-class SignUp(CreateView):
+
+class SignUp(SuccessMessageMixin, CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "users/signup.html" 
+    success_message = "You have successfully signed up!"
  
