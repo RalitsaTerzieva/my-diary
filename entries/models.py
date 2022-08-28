74f32166216afd0e8ef5b492fd9ec301d7e5311b
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2022-08-28 15:21:38 +0300

    Add FK to entry model

--- entries/models.py
+++ entries/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Entry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
     date_created = models.DateTimeField(default=timezone.now)
